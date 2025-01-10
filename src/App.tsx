03474b9a8c05f4baaff860dba6156ec21c236499
clevrf0x <favasm72@gmail.com>
2025-01-11 05:19:20 +0530

    init

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+// import { ModeToggle } from './components/mode-toggle';
+
+function App() {
+  return (
+    <React.Fragment>
+      {/*  */}
+      {/* <ModeToggle /> */}
+    </React.Fragment>
+  );
+}
+
+export default App;
